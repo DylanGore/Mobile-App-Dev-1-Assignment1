02c5effec4791f8f15937f482266b67d81b6b9e5
Dylan Gore <hello@dylangore.ie>
2020-10-10 10:45:46 +0100

    Initial code commit

---
+++ src/main/kotlin/ie/dylangore/mad1/assignment1/main.kt
@@ -0,0 +1,9 @@
+package ie.dylangore.mad1.assignment1
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    logger.info { "Mobile App Dev 1 Assignment 1" }
+}
